d7c15f2ea87ce3812b4cb452c4be83a56ecf5565
Akira Kubo <a_kubo99@yahoo.co.jp>
2018-04-11 14:11:14 +0900

    ﻿:+1:添付ビヘイビアの追加
    Commit body...

---
+++ IntegerUpDownBehaviors.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using Xceed.Wpf.Toolkit;
+
+namespace NumericalUpDownSample
+{
+    public class IntegerUpDownBehaviors
+    {
+        /// <summary>
+        /// True なら入力を数字のみに制限します。
+        /// </summary>
+        public static readonly DependencyProperty IsNumericProperty =
+                    DependencyProperty.RegisterAttached(
+                        "IsNumeric", typeof(bool),
+                        typeof(IntegerUpDownBehaviors),
+                        new UIPropertyMetadata(false, IsNumericChanged)
+                    );
+
+        [AttachedPropertyBrowsableForType(typeof(IntegerUpDown))]
+        public static bool GetIsNumeric(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsNumericProperty);
+        }
+
+        [AttachedPropertyBrowsableForType(typeof(IntegerUpDownBehaviors))]
+        public static void SetIsNumeric(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsNumericProperty, value);
+        }
+
+        private static void IsNumericChanged
+            (DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+
+            var integerUpDown = sender as IntegerUpDown;
+            if (integerUpDown == null) return;
+
+            // イベントを登録・削除 
+            integerUpDown.PreviewTextInput -= OnPreviewTextInput;
+            DataObject.RemovePastingHandler(integerUpDown, textbox_PastingHandler);
+            
+            var newValue = (bool)e.NewValue;
+            if (newValue)
+            {
+                integerUpDown.PreviewTextInput += OnPreviewTextInput;
+                DataObject.AddPastingHandler(integerUpDown, textbox_PastingHandler);
+
+                var textBox = integerUpDown.FindName("PART_TextBox") as WatermarkTextBox;
+                if (null != textBox)
+                {
+                    InputMethod.SetIsInputMethodSuspended(textBox, true);
+                }
+
+            }
+        }
+
+
+        private static bool IsAllNumber(string text)
+        {
+            return !text.Any(c => !char.IsNumber(c));
+        }
+        private static void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!IsAllNumber(e.Text))
+            {
+                e.Handled = true;
+            }
+        }
+        //ペーストに対応するために必要
+        private static void textbox_PastingHandler(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(DataFormats.Text))
+            {
+                string text = Convert.ToString(e.DataObject.GetData(DataFormats.Text));
+                if (!IsAllNumber(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
+        }
+    }
+}
